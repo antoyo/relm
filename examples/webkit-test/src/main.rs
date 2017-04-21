3f1a9efee0a2bf3d6e82fa9fb4d3924ed1748746
Antoni Boucher <bouanto@zoho.com>
2017-04-20 22:06:51 -0400

    Add example using external gtk widget

---
+++ examples/webkit-test/src/main.rs
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2017 Boucher, Antoni <bouanto@zoho.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#![feature(proc_macro)]
+
+extern crate gtk;
+#[macro_use]
+extern crate relm;
+extern crate relm_attributes;
+#[macro_use]
+extern crate relm_derive;
+extern crate webkit2gtk;
+
+use gtk::{
+    Inhibit,
+    WidgetExt,
+};
+use relm::Widget;
+use relm_attributes::widget;
+
+use self::Msg::*;
+
+#[derive(Clone)]
+pub struct Model {
+    counter: i32,
+}
+
+#[derive(Msg)]
+pub enum Msg {
+    Quit,
+}
+
+#[widget]
+impl Widget for Win {
+    fn init_view(&self) {
+        self.webview.load_uri("https://crates.io/");
+    }
+
+    fn model() -> Model {
+        Model {
+            counter: 0,
+        }
+    }
+
+    fn update(&mut self, event: Msg, _model: &mut Model) {
+        match event {
+            Quit => gtk::main_quit(),
+        }
+    }
+
+    view! {
+        gtk::Window {
+            #[name="webview"]
+            webkit2gtk::WebView {
+            },
+            delete_event(_, _) => (Quit, Inhibit(false)),
+        }
+    }
+}
+
+fn main() {
+    relm::run::<Win>().unwrap();
+}
