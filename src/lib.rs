3fc2109ae43116cd3ce83f0cc9bdf5f3d46ac84a
Antoni Boucher <bouanto@zoho.com>
2017-04-26 18:03:22 -0400

    Add TODO

--- src/lib.rs
+++ src/lib.rs
@@ -40,6 +40,7 @@
 #![warn(missing_docs, trivial_casts, trivial_numeric_casts, unused_extern_crates, unused_import_braces, unused_qualifications, unused_results)]
 
 /*
+ * TODO: support msg variant with multiple values?
    TODO: after switching to futures-glib, remove the unnecessary Arc, Mutex and Clone.
  * FIXME: the widget-list example can trigger (and is broken) the following after removing widgets, adding new
  * widgets again and using these new widgets:
