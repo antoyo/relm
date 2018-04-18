a5cd058d38b46f9b993a9cfe05218b067b3416f9
Antoni Boucher <bouanto@zoho.com>
2018-04-18 10:07:04 -0400

    Add test for grid

--- tests/grid-attributes.rs
+++ tests/grid-attributes.rs
@@ -27,6 +27,7 @@ extern crate relm;
 extern crate relm_attributes;
 #[macro_use]
 extern crate relm_derive;
+extern crate relm_test;
 
 use gtk::{
     ButtonExt,
@@ -58,6 +59,7 @@ impl Widget for Win {
     view! {
         gtk::Window {
             gtk::Grid {
+                #[name="button7"]
                 gtk::Button {
                     label: "7",
                     cell: {
@@ -65,6 +67,7 @@ impl Widget for Win {
                         top_attach: 0,
                     },
                 },
+                #[name="button8"]
                 gtk::Button {
                     label: "8",
                     cell: {
@@ -72,6 +75,7 @@ impl Widget for Win {
                         top_attach: 0,
                     },
                 },
+                #[name="button9"]
                 gtk::Button {
                     label: "9",
                     cell: {
@@ -79,6 +83,7 @@ impl Widget for Win {
                         top_attach: 0,
                     },
                 },
+                #[name="button4"]
                 gtk::Button {
                     label: "4",
                     cell: {
@@ -86,6 +91,7 @@ impl Widget for Win {
                         top_attach: 1,
                     },
                 },
+                #[name="button5"]
                 gtk::Button {
                     label: "5",
                     cell: {
@@ -93,6 +99,7 @@ impl Widget for Win {
                         top_attach: 1,
                     },
                 },
+                #[name="button6"]
                 gtk::Button {
                     label: "6",
                     cell: {
@@ -100,6 +107,7 @@ impl Widget for Win {
                         top_attach: 1,
                     },
                 },
+                #[name="button1"]
                 gtk::Button {
                     label: "1",
                     cell: {
@@ -107,6 +115,7 @@ impl Widget for Win {
                         top_attach: 2,
                     },
                 },
+                #[name="button2"]
                 gtk::Button {
                     label: "2",
                     cell: {
@@ -114,6 +123,7 @@ impl Widget for Win {
                         top_attach: 2,
                     },
                 },
+                #[name="button3"]
                 gtk::Button {
                     label: "3",
                     cell: {
@@ -121,6 +131,7 @@ impl Widget for Win {
                         top_attach: 2,
                     },
                 },
+                #[name="button_plus_minus"]
                 gtk::Button {
                     label: "+/-",
                     cell: {
@@ -128,6 +139,7 @@ impl Widget for Win {
                         top_attach: 3,
                     },
                 },
+                #[name="button0"]
                 gtk::Button {
                     label: "0",
                     cell: {
@@ -135,6 +147,7 @@ impl Widget for Win {
                         top_attach: 3,
                     },
                 },
+                #[name="button_dot"]
                 gtk::Button {
                     label: ".",
                     cell: {
@@ -150,4 +163,69 @@ impl Widget for Win {
 
 fn main() {
     Win::run(()).unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use gtk::WidgetExt;
+
+    use relm;
+
+    use Win;
+
+    #[test]
+    fn widget_position() {
+        let (_component, widgets) = relm::init_test::<Win>(()).unwrap();
+        let button7 = &widgets.button7;
+        let button8 = &widgets.button8;
+        let button9 = &widgets.button9;
+        let button4 = &widgets.button4;
+        let button5 = &widgets.button5;
+        let button6 = &widgets.button6;
+        let button1 = &widgets.button1;
+        let button2 = &widgets.button2;
+        let button3 = &widgets.button3;
+        let button_plus_minus = &widgets.button_plus_minus;
+        let button0 = &widgets.button0;
+        let button_dot = &widgets.button_dot;
+
+        let button7_allocation = button7.get_allocation();
+        let button8_allocation = button8.get_allocation();
+        let button9_allocation = button9.get_allocation();
+        let button4_allocation = button4.get_allocation();
+        let button5_allocation = button5.get_allocation();
+        let button6_allocation = button6.get_allocation();
+        let button1_allocation = button1.get_allocation();
+        let button2_allocation = button2.get_allocation();
+        let button3_allocation = button3.get_allocation();
+        let button_pm_allocation = button_plus_minus.get_allocation();
+        let button0_allocation = button0.get_allocation();
+        let button_dot_allocation = button_dot.get_allocation();
+        assert!(button7_allocation.x < button8_allocation.x);
+        assert_eq!(button7_allocation.x, button4_allocation.x);
+        assert!(button7_allocation.y < button4_allocation.y);
+        assert!(button8_allocation.x < button9_allocation.x);
+        assert_eq!(button8_allocation.x, button5_allocation.x);
+        assert!(button8_allocation.y < button5_allocation.y);
+        assert_eq!(button9_allocation.x, button6_allocation.x);
+        assert!(button9_allocation.y < button6_allocation.y);
+
+        assert!(button4_allocation.x < button5_allocation.x);
+        assert_eq!(button4_allocation.x, button1_allocation.x);
+        assert!(button4_allocation.y < button1_allocation.y);
+        assert!(button5_allocation.x < button6_allocation.x);
+        assert_eq!(button5_allocation.x, button2_allocation.x);
+        assert!(button5_allocation.y < button2_allocation.y);
+        assert_eq!(button6_allocation.x, button3_allocation.x);
+        assert!(button6_allocation.y < button3_allocation.y);
+
+        assert!(button1_allocation.x < button2_allocation.x);
+        assert_eq!(button1_allocation.x, button_pm_allocation.x);
+        assert!(button1_allocation.y < button_pm_allocation.y);
+        assert!(button2_allocation.x < button3_allocation.x);
+        assert_eq!(button2_allocation.x, button0_allocation.x);
+        assert!(button2_allocation.y < button0_allocation.y);
+        assert_eq!(button3_allocation.x, button_dot_allocation.x);
+        assert!(button3_allocation.y < button_dot_allocation.y);
+    }
 }
