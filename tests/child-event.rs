5b1ad86a9bf52caff7151573c7254c897b37152b
Antoni Boucher <bouanto@zoho.com>
2018-04-18 10:25:33 -0400

    Update date in copyleft

--- tests/child-event.rs
+++ tests/child-event.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Boucher, Antoni <bouanto@zoho.com>
+ * Copyright (c) 2017-2018 Boucher, Antoni <bouanto@zoho.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
