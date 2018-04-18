03035c3721ad81b7a34880b8bee86deb209fa863
Antoni Boucher <bouanto@zoho.com>
2018-04-18 11:19:57 -0400

    Add test for key-events

--- tests/key-events-attribute.rs
+++ tests/key-events-attribute.rs
@@ -27,6 +27,8 @@ extern crate relm;
 extern crate relm_attributes;
 #[macro_use]
 extern crate relm_derive;
+#[macro_use]
+extern crate relm_test;
 
 use std::cell::Cell;
 use std::rc::Rc;
@@ -42,7 +44,7 @@ use self::Msg::*;
 
 #[derive(Msg)]
 pub enum Msg {
-    Delete,
+    KeyPress,
     Press,
     Release,
     Quit,
@@ -64,11 +66,7 @@ impl Widget for Win {
 
     fn update(&mut self, event: Msg) {
         match event {
-            Delete => {
-                if self.model.press_count.get() <= 3 {
-                    self.model.relm.stream().emit(Quit);
-                }
-            },
+            KeyPress => (),
             Press => {
                 self.model.press_count.set(self.model.press_count.get() + 1);
                 println!("Press");
@@ -81,18 +79,59 @@ impl Widget for Win {
     }
 
     view! {
+        #[name="window"]
         gtk::Window {
+            gtk::Box {
+                #[name="entry"]
+                gtk::Entry {
+                    key_press_event(_, key) with (press_count, relm) => (KeyPress, inhibit_press_event(&press_count, &relm)),
+                },
+            },
             key_press_event(_, key) => (Press, Inhibit(false)),
             key_release_event(_, key) => (Release, Inhibit(false)),
-            delete_event(_, _) with (press_count, relm) => (Delete, inhibit_delete_event(&press_count, &relm)),
+            delete_event(_, _) => (Quit, Inhibit(false)),
         }
     }
 }
 
-fn inhibit_delete_event(press_count: &Rc<Cell<i32>>, _relm: &Relm<Win>) -> Inhibit {
+fn inhibit_press_event(press_count: &Rc<Cell<i32>>, _relm: &Relm<Win>) -> Inhibit {
     Inhibit(press_count.get() > 3)
 }
 
 fn main() {
     Win::run(()).unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use gtk::EntryExt;
+
+    use relm;
+    use relm_test::enter_keys;
+
+    use Win;
+
+    #[test]
+    fn inhibit_event() {
+        let (_component, widgets) = relm::init_test::<Win>(()).unwrap();
+        let entry = &widgets.entry;
+
+        enter_keys(entry, "a");
+        assert_text!(entry, "a");
+
+        enter_keys(entry, "b");
+        assert_text!(entry, "ab");
+
+        enter_keys(entry, "c");
+        assert_text!(entry, "abc");
+
+        enter_keys(entry, "d");
+        assert_text!(entry, "abcd");
+
+        enter_keys(entry, "e");
+        assert_text!(entry, "abcd");
+
+        enter_keys(entry, "f");
+        assert_text!(entry, "abcd");
+    }
 }
--- tests/key-events.rs
+++ tests/key-events.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Boucher, Antoni <bouanto@zoho.com>
+ * Copyright (c) 2017-2018 Boucher, Antoni <bouanto@zoho.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -24,33 +24,45 @@ extern crate gtk;
 extern crate relm;
 #[macro_use]
 extern crate relm_derive;
+#[macro_use]
+extern crate relm_test;
+
+use std::cell::Cell;
+use std::rc::Rc;
 
 use gtk::{
+    ContainerExt,
+    Entry,
     Inhibit,
     WidgetExt,
     Window,
     WindowType,
 };
-use relm::{Relm, Update, Widget};
+use relm::{Relm, Update, Widget, WidgetTest};
 
 use self::Msg::*;
 
 #[derive(Msg)]
 pub enum Msg {
-    Delete,
+    KeyPress,
     Press,
     Release,
     Quit,
 }
 
 pub struct Model {
-    press_count: i32,
-    relm: Relm<Win>,
+    press_count: Rc<Cell<i32>>,
+}
+
+#[derive(Clone)]
+struct Widgets {
+    entry: Entry,
+    window: Window,
 }
 
 struct Win {
     model: Model,
-    window: Window,
+    widgets: Widgets,
 }
 
 impl Update for Win {
@@ -58,22 +70,17 @@ impl Update for Win {
     type ModelParam = ();
     type Msg = Msg;
 
-    fn model(relm: &Relm<Self>, _: ()) -> Model {
+    fn model(_relm: &Relm<Self>, _: ()) -> Model {
         Model {
-            press_count: 0,
-            relm: relm.clone(),
+            press_count: Rc::new(Cell::new(0)),
         }
     }
 
     fn update(&mut self, event: Msg) {
         match event {
-            Delete => {
-                if self.model.press_count <= 3 {
-                    self.model.relm.stream().emit(Quit);
-                }
-            },
+            KeyPress => (),
             Press => {
-                self.model.press_count += 1;
+                self.model.press_count.set(self.model.press_count.get() + 1);
                 println!("Press");
             },
             Release => {
@@ -88,25 +95,80 @@ impl Widget for Win {
     type Root = Window;
 
     fn root(&self) -> Self::Root {
-        self.window.clone()
+        self.widgets.window.clone()
     }
 
     fn view(relm: &Relm<Win>, model: Self::Model) -> Self {
         let window = Window::new(WindowType::Toplevel);
+        let entry = Entry::new();
+        window.add(&entry);
+
+        let press_count = model.press_count.clone();
+        let cloned_relm = relm.clone();
+        connect!(relm, entry, connect_key_press_event(_, _), return (KeyPress, inhibit_press_event(&press_count, &cloned_relm)));
 
         window.show_all();
 
         connect!(relm, window, connect_key_press_event(_, _), return (Press, Inhibit(false)));
         connect!(relm, window, connect_key_release_event(_, _), return (Release, Inhibit(false)));
-        connect!(relm, window, connect_delete_event(_, _), return (Delete, Inhibit(true)));
+        connect!(relm, window, connect_delete_event(_, _), return (Quit, Inhibit(false)));
 
         Win {
             model,
-            window,
+            widgets: Widgets {
+                entry,
+                window,
+            },
         }
     }
 }
 
+impl WidgetTest for Win {
+    type Widgets = Widgets;
+
+    fn get_widgets(&self) -> Self::Widgets {
+        self.widgets.clone()
+    }
+}
+
+fn inhibit_press_event(press_count: &Rc<Cell<i32>>, _relm: &Relm<Win>) -> Inhibit {
+    Inhibit(press_count.get() > 3)
+}
+
 fn main() {
     Win::run(()).unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use gtk::EntryExt;
+
+    use relm;
+    use relm_test::enter_keys;
+
+    use Win;
+
+    #[test]
+    fn inhibit_event() {
+        let (_component, widgets) = relm::init_test::<Win>(()).unwrap();
+        let entry = &widgets.entry;
+
+        enter_keys(entry, "a");
+        assert_text!(entry, "a");
+
+        enter_keys(entry, "b");
+        assert_text!(entry, "ab");
+
+        enter_keys(entry, "c");
+        assert_text!(entry, "abc");
+
+        enter_keys(entry, "d");
+        assert_text!(entry, "abcd");
+
+        enter_keys(entry, "e");
+        assert_text!(entry, "abcd");
+
+        enter_keys(entry, "f");
+        assert_text!(entry, "abcd");
+    }
 }
