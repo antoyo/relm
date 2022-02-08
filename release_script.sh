a0a7e97f251f9af057738c409b3e0f220e85b0de
Antoni Boucher <bouanto@zoho.com>
2022-02-08 18:09:53 -0500

    Fix release script

--- release_script.sh
+++ release_script.sh
@@ -4,12 +4,12 @@ git pull
 git push
 
 cd ./relm-derive
-cargo release --no-dev-version
+cargo release --no-dev-version --execute
 cd ..
 
-cargo release --no-dev-version
+cargo release --no-dev-version --execute
 git push
 
 cd ./relm-test
-cargo release --no-dev-version
+cargo release --no-dev-version --execute
 cd ..
