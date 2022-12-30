a955930f0be0bc441b69b4edc545c1bc0a5b2b92
Dominic <git@msrd0.de>
2022-12-30 21:25:48 +0100

    Suppress unknown lints

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,7 @@
 //!
 //! More info can be found in the [readme](https://github.com/antoyo/relm#relm).
 
-#![allow(clippy::new_without_default, clippy::uninlined_format_args)]
+#![allow(clippy::new_without_default, clippy::uninlined_format_args, unknown_lints)]
 
 #![warn(
     missing_docs,
